9fd4b341a176331cde6fa3677a15431b8d660abc
vi0650 <vaibhavvpadmani650@gmail.com>
2025-11-21 23:21:04 +0530

    published

---
+++ task-5/js.js
@@ -0,0 +1,6 @@
+var a = undefined;
+console.log(a);
+
+a=12;
+
+// // obj this is all about hoisting;
---
+++ task-6/js.js
@@ -0,0 +1,5 @@
+document.querySelector("h1").style.Color = "red";
+document.querySelector("h1").style.backgroundColor = "green";
+
+let gender  = "male,female,trans";
+let a = console.log
